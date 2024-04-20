9b9c56b83e0f0c8a1a5a97c90fe8aa5f9d62b63c
shifavoila <d4@voilacode.com>
2024-04-20 11:09:57 +0530

    uploading without views

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 use App\Models\User;
 
 class AuthController extends Controller
@@ -21,24 +22,29 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
-        $credentials = request(['email', 'password']);
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
 
-        if (!Auth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if(auth()->attempt($data)) {
+            $token = auth()->user()->createToken('Token')->accessToken;
+
+            return response()->json(['token'=>$token], 200);
+        } else {
+            return response()->json(['error'=>"Unauthorized Access"], 401);
         }
-
-        $user = $request->user();
-        $token = $user->createToken('Token Name')->accessToken;
-
-        return response()->json(['token' => $token], 200);
     }
 
     public function user(Request $request) {
-        return response()->json(['user' => $request->user()], 200);
+        $user = auth()->user();
+        return response()->json(['user'=>$user],200);
     }
 
     public function logout(Request $request) {
         $request->user()->token()->revoke();
+    
         return response()->json(['message' => 'Successfully logged out'], 200);
     }
+    
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,6 @@
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -28,7 +27,6 @@
                             name="password"
                             required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
